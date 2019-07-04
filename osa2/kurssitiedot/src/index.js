6cdd7505011ce30f18464977322941298597ecd0
Atte Laakso <laakso95@gmail.com>
2019-07-04 09:25:47 +0300

    Exercise 2.1

---
+++ osa2/kurssitiedot/src/index.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const Header = ({ title }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  )
+}
+
+const Content = ({ parts }) => {
+
+  const rows = () => parts.map(part =>
+    <Part
+      key={part.id}
+      name={part.name}
+      exercises={part.exercises}
+    />
+  )
+
+  return (
+    <div>
+      {rows()}
+    </div>
+  )
+}
+
+const Part = ({ name, exercises }) => {
+  return (
+    <div>
+      <p>{name} {exercises}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 0 // hard coded ids...
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 1
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 2
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
