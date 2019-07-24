b673e600801eb86549cb85b58bf2436d3157127b
Atte Laakso <laakso95@gmail.com>
2019-07-24 12:54:38 +0300

    Exercise 5.21

--- osa5/custom-hooks/src/App.js
+++ osa5/custom-hooks/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-
 const useField = (type) => {
   const [value, setValue] = useState('')
 
@@ -19,10 +18,27 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    axios
+      .get(baseUrl)
+      .then(response => {
+        setResources(response.data)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }, [baseUrl])
 
   const create = (resource) => {
-    // ...
+    console.log(resource)
+    axios
+      .post(baseUrl, resource)
+      .then(response => {
+        setResources(resources.concat(response.data))
+      })
+      .catch(error => {
+        console.error(error)
+      })
   }
 
   const service = {
@@ -46,10 +62,10 @@ const App = () => {
     event.preventDefault()
     noteService.create({ content: content.value })
   }
- 
+
   const handlePersonSubmit = (event) => {
     event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
+    personService.create({ name: name.value, number: number.value })
   }
 
   return (
@@ -63,7 +79,7 @@ const App = () => {
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
+        name <input {...name} /> <br />
         number <input {...number} />
         <button>create</button>
       </form>
