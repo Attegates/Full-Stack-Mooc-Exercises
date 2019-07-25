4d684ab0894d8cd4e061a34e25f21ebeac01708d
Atte Laakso <laakso95@gmail.com>
2019-07-25 09:29:26 +0300

    Exercise 6.1

--- osa6/unicafe-redux/src/reducer.test.js
+++ osa6/unicafe-redux/src/reducer.test.js
@@ -32,4 +32,57 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('ZERO resets the state', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+
+    deepFreeze(newState)
+    const resetedState = counterReducer(state, { type: 'ZERO' })
+    expect(resetedState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+
+  })
+
 })
