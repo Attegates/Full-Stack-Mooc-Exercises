251451a0a0fcf6535b9e7b1d417b27c90af4bd24
Atte Laakso <laakso95@gmail.com>
2019-07-26 12:54:17 +0300

    Exercise 6.16

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -2,8 +2,17 @@ import React from 'react';
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
+import { useEffect } from 'react'
+import anecdoteService from './services/anecdoteService'
+import { initAnecdotes } from './reducers/anecdoteReducer'
+import { connect } from 'react-redux'
 
 const App = (props) => {
+
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdotes => props.initAnecdotes(anecdotes))
+  })
+
   return (
     <div>
       <h2>Anecdotes</h2>
@@ -14,4 +23,4 @@ const App = (props) => {
   )
 }
 
-export default App
+export default connect(null, { initAnecdotes })(App)
--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,12 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -17,9 +8,7 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
-const anecdoteReducer = (state = initialState, action) => {
+const anecdoteReducer = (state = [], action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
@@ -31,6 +20,8 @@ const anecdoteReducer = (state = initialState, action) => {
       const toChange = state.find(a => a.id === id)
       const changed = { ...toChange, votes: toChange.votes + 1 }
       return state.map(a => a.id !== id ? a : changed)
+    case 'INIT_ANECDOTES':
+      return action.data
     default:
       return state
   }
@@ -50,5 +41,12 @@ const createAnecdote = (content) => {
   }
 }
 
-export { addVote, createAnecdote }
+const initAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT_ANECDOTES',
+    data: anecdotes,
+  }
+}
+
+export { addVote, createAnecdote, initAnecdotes }
 export default anecdoteReducer
